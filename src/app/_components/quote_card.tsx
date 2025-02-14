a4e40ab41992bca81867fed2d7c65e46cb8a8b20
minh.le <kuro.keita94@gmail.com>
2025-02-15 01:03:51 +0700

    look better now?

--- src/app/_components/quote_card.tsx
+++ src/app/_components/quote_card.tsx
@@ -55,72 +55,65 @@ export default function QuoteCard() {
 			</div>
 			<div className='backdrop-blur-sm border-t rounded-b-xl py-3 px-4 md:py-4 md:px-5 dark:border-neutral-700 group'>
 				<div className='flex justify-between items-center'>
-					<button
-						onClick={fetchQuote}
-						className='py-2 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'
-					>
-						<svg
-							className='w-4 h-4'
-							xmlns='http://www.w3.org/2000/svg'
-							fill='none'
-							viewBox='0 0 24 24'
-							strokeWidth={2}
-							stroke='currentColor'
-						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								d='M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99'
-							/>
-						</svg>
-					</button>
-					<p
-						className='hs-collapse-toggle mt-1 text-sm text-gray-500 dark:text-white cursor-pointer peer'
-						id='author-description-collapse'
-						aria-expanded='false'
-						aria-controls='author-description-collapse-heading'
-						data-hs-collapse='#author-description-collapse-heading'
-					>
-						{quote.author.name}
-					</p>
-				</div>
-				<div
-					id='author-description-collapse-heading'
-					className='hs-collapse hidden w-full max-h-48 transition-[height] duration-300 text-justify overflow-hidden peer-aria-expanded:overflow-y-auto'
-					aria-labelledby='author-description-collapse'
-				>
-					<div className='mt-5'>
-						<p className='text-gray-500 dark:text-neutral-400'>{quote.author.description}</p>
-						<div className='group'>
-							<button
-								type='button'
-								id='author-bio-collapse'
-								className='hs-collapse-toggle inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 decoration-2 hover:text-blue-700 hover:underline focus:outline-none focus:underline focus:text-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-600 dark:focus:text-blue-600'
-								data-hs-collapse='#author-bio-collapse-heading'
-							>
-								<span className='[&.open]:hidden'>Read more</span>
-								<span className='[&.open]:block hidden'>Read less</span>
-								<svg
-									className='transition-transform duration-300 [.open]:rotate-180 flex-shrink-0 w-4 h-4'
-									xmlns='http://www.w3.org/2000/svg'
-									width='24'
-									height='24'
-									viewBox='0 0 24 24'
-									fill='none'
-									stroke='currentColor'
-									strokeWidth='2'
-									strokeLinecap='round'
-									strokeLinejoin='round'
+					<div className='hs-accordion-group w-full'>
+						<div className='w-full hs-accordion active' id='hs-basic-nested-heading-one'>
+							<div className='flex justify-between items-center'>
+								<button
+									onClick={fetchQuote}
+									className='py-2 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'
 								>
-									<path d='m6 9 6 6 6-6' />
-								</svg>
-							</button>
+									<svg
+										className='w-4 h-4'
+										xmlns='http://www.w3.org/2000/svg'
+										fill='none'
+										viewBox='0 0 24 24'
+										strokeWidth={2}
+										stroke='currentColor'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											d='M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99'
+										/>
+									</svg>
+								</button>
+								<button
+									className='hs-accordion-toggle hs-accordion-active:text-blue-600 py-3 inline-flex items-center gap-x-3 font-semibold text-end text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400'
+									aria-expanded='true'
+									aria-controls='hs-basic-nested-collapse-one'
+								>
+									{quote.author.name}
+								</button>
+							</div>
 							<div
-								id='author-bio-collapse-heading'
-								className='hs-collapse hidden w-full max-h-48 transition-[height] duration-300 text-justify overflow-hidden peer-aria-expanded:overflow-y-auto author-bio-collapse-heading bio-test'
-								aria-labelledby='author-bio-collapse'
+								id='hs-basic-nested-collapse-one'
+								className='hs-accordion-content mt-2 w-full overflow-hidden transition-[height] duration-300'
+								role='region'
+								aria-labelledby='hs-basic-nested-heading-one'
 							>
-								<p className='text-gray-500 dark:text-neutral-400'>{quote.author.bio}</p>
+								<div className='hs-accordion-group px-0'>
+									<div className='hs-accordion active' id='hs-basic-nested-sub-heading-one'>
+										<p>{quote.author.description}</p>
+										<button
+											className='hs-accordion-toggle py-3 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400'
+											aria-controls='hs-basic-nested-sub-collapse-one'
+										>
+											<span className='hs-accordion-active:hidden text-blue-600 dark:text-blue-500'>More</span>
+											<span className='hidden hs-accordion-active:block text-gray-800 dark:text-neutral-400'>Less</span>
+										</button>
+										<div
+											id='hs-basic-nested-sub-collapse-one'
+											className='hs-accordion-content w-full overflow-hidden transition-[height] duration-300'
+											role='region'
+											aria-labelledby='hs-basic-nested-sub-heading-one'
+										>
+											<p className='text-gray-800 dark:text-neutral-200'>{quote.author.bio}</p>
+											<a href={quote.author.link} className='text-blue-600 decoration-blue-500 dark:text-blue-500'>
+												{quote.author.link}
+											</a>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
