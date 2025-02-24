31ec458e82ba2c987b87e42530285ab4aebe7909
minh.le <kuro.keita94@gmail.com>
2025-02-24 18:42:46 +0700

    fix linting errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,11 @@
 import QuoteCard from '@/app/_components/quote_card'
 import { healthCheck } from '@/common/utils/api'
 
-type HomeQueryParams = {
-	author?: string
-	query?: string
-}
-
-export default async function Home({ searchParams }: { searchParams: Promise<HomeQueryParams> }) {
+export default async function Home() {
 	await healthCheck()
 
 	return (
 		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-			{/* <div className='fixed top-0 left-0 right-0 z-50 bg-white/80 dark:bg-neutral-900/80 backdrop-blur-lg shadow-md'>
-				<div className='max-w-2xl mx-auto p-4'>
-					<form className='w-full max-w-xl mx-auto flex justify-center gap-4' action={() => {}}>
-						<div className='relative flex-1'>
-							<input
-								type='text'
-								id='hs-floating-underline-input-author'
-								className='peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 text-sm placeholder:text-transparent focus:border-t-transparent focus:border-x-transparent focus:border-b-blue-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none dark:border-b-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600 dark:focus:border-b-neutral-600
-    focus:pt-6
-    focus:pb-2
-    [&:not(:placeholder-shown)]:pt-6
-    [&:not(:placeholder-shown)]:pb-2
-    autofill:pt-6
-    autofill:pb-2'
-								placeholder='Author'
-								name='author'
-							/>
-							<label
-								htmlFor='hs-floating-underline-input-author'
-								className='absolute top-0 start-0 py-4 px-0 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none
-      peer-focus:scale-90
-      peer-focus:translate-x-0.5
-      peer-focus:-translate-y-1.5
-      peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
-      peer-[:not(:placeholder-shown)]:scale-90
-      peer-[:not(:placeholder-shown)]:translate-x-0.5
-      peer-[:not(:placeholder-shown)]:-translate-y-1.5
-      peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-white dark:text-white'
-							>
-								Author
-							</label>
-						</div>
-
-						<div className='relative flex-1'>
-							<input
-								type='text'
-								id='hs-floating-underline-input-content'
-								className='peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 text-sm placeholder:text-transparent focus:border-t-transparent focus:border-x-transparent focus:border-b-blue-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none dark:border-b-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600 dark:focus:border-b-neutral-600
-    focus:pt-6
-    focus:pb-2
-    [&:not(:placeholder-shown)]:pt-6
-    [&:not(:placeholder-shown)]:pb-2
-    autofill:pt-6
-    autofill:pb-2'
-								placeholder='Content'
-								name='query'
-							/>
-							<label
-								htmlFor='hs-floating-underline-input-content'
-								className='absolute top-0 start-0 py-4 px-0 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none
-      peer-focus:scale-90
-      peer-focus:translate-x-0.5
-      peer-focus:-translate-y-1.5
-      peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
-      peer-[:not(:placeholder-shown)]:scale-90
-      peer-[:not(:placeholder-shown)]:translate-x-0.5
-      peer-[:not(:placeholder-shown)]:-translate-y-1.5
-      peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-white dark:text-white'
-							>
-								Content
-							</label>
-						</div>
-
-						<div className='relative flex justify-center justify-items-center'>
-							<button
-								className='py-2 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'
-								type='submit'
-							>
-								Search
-							</button>
-						</div>
-					</form>
-				</div>
-			</div> */}
-
 			<main className='flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-2xl mx-auto'>
 				<QuoteCard />
 			</main>
