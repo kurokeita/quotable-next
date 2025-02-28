02915810371061220c5d24a3c2edc0ec7f1e3ab2
minh.le <kuro.keita94@gmail.com>
2025-02-28 12:21:25 +0700

    update the authors table

--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -3,7 +3,7 @@ import { Card } from '@/components/ui/card'
 
 export default function AuthorsPage() {
 	return (
-		<div className='grow flex flex-col justify-center items-center min-h-auto p-0 pb-8 lg:p-8 lg:pb-20 w-full gap-16 sm:p-20 min-w-fit'>
+		<div className='grow flex flex-col justify-center items-center min-h-auto p-0 pb-8 lg:p-8 lg:pb-20 w-full gap-16 sm:p-20 max-w-screen'>
 			<Card className='bg-black/15 backdrop-blur-xl drop-shadow-md w-full border-transparent text-current'>
 				<AuthorsTable />
 			</Card>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 	await healthCheck()
 
 	return (
-		<div className='grow flex flex-col justify-center min-h-auto p-0 pb-8 lg:p-8 lg:pb-20 w-full gap-16 sm:p-20 min-w-fit'>
+		<div className='grow flex flex-col justify-center min-h-auto p-0 pb-8 lg:p-8 lg:pb-20 w-full gap-16 sm:p-20 min-w-fit max-w-screen'>
 			<main className='flex flex-col gap-8 items-center lg:items-start xl:items-center w-full lg:w-1/2 mx-auto'>
 				<QuoteCard />
 			</main>
