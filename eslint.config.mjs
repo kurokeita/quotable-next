38bb63bc3bcee0fb674b2c330eb1f09d4a2f5114
minh.le <kuro.keita94@gmail.com>
2025-02-28 15:57:45 +0700

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
 	...compat.extends('next/core-web-vitals', 'next/typescript'),
 	{
+		linterOptions: {
+			reportUnusedDisableDirectives: 'error',
+		},
 		rules: {
 			semi: ['off'],
 			curly: ['error', 'all'],
