88abab8f282d058b3cd544874c5580adffa0185c
kurokeita <kuro.keita94@gmail.com>
2025-03-24 11:04:37 +0700

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import QuoteCard from "@/app/_components/quote-card";
-import { healthCheck } from "@/common/utils/api";
+import QuoteCard from '@/app/_components/quote-card'
+import { healthCheck } from '@/common/utils/api'
 
 export default async function Home() {
-  await healthCheck();
+	await healthCheck()
 
-  return (
-    <div className="grow flex flex-col justify-center min-h-auto p-0 sm:p-8 xl:p-20 w-full gap-16 min-w-fit max-w-screen">
-      <main className="flex flex-col grow sm:grow-0 gap-8 items-center lg:items-start xl:items-center w-full lg:w-1/2 mx-auto rounded-none sm:rounded-md justify-center">
-        <QuoteCard />
-      </main>
-    </div>
-  );
+	return (
+		<div className='grow flex flex-col justify-center min-h-auto p-0 sm:p-8 xl:p-20 w-full gap-16 min-w-fit max-w-screen'>
+			<main className='flex flex-col grow sm:grow-0 gap-8 items-center lg:items-start xl:items-center w-full lg:w-1/2 mx-auto rounded-none sm:rounded-md justify-center'>
+				<QuoteCard />
+			</main>
+		</div>
+	)
 }
